eb8ccb4bc5b6c30a48860c3d7c675e7b487e8cd9
sanwalkailash <sanwalkailash@gmail.com>
2019-05-23 10:35:07 +0530

    config

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI:process.env.MONGO_URI,
-  googleClientID:process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret:process.env.GOOGLE_CLIENT_SECRET
+  mongoURI:"mongodb+srv://demo:<password>@cluster0-n944i.mongodb.net/test?retryWrites=true",
+  googleClientID:"107698569510-edomo02jgeqr6k4hcqtl4kdhevnvrj17.apps.googleusercontent.com",
+  googleClientSecret:"jf3tFbZE1-WXMRRyqaBGFf9w"
 }
