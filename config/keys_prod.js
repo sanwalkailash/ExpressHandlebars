2ca6fe1d4102f39f26567ecb71fe1ac64757cbfb
sanwalkailash <sanwalkailash@gmail.com>
2019-05-23 10:46:04 +0530

    wip

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI:"mongodb+srv://demo:<password>@cluster0-n944i.mongodb.net/test?retryWrites=true",
+  mongoURI:"mongodb+srv://demo:demo@cluster0-n944i.mongodb.net/test?retryWrites=true\n",
   googleClientID:"107698569510-edomo02jgeqr6k4hcqtl4kdhevnvrj17.apps.googleusercontent.com",
   googleClientSecret:"jf3tFbZE1-WXMRRyqaBGFf9w"
 }
