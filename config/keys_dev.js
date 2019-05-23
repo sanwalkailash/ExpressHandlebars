aa5e646c886b1fa5bcdcfa388210c3c6d851984d
sanwalkailash <sanwalkailash@gmail.com>
2019-05-23 10:38:35 +0530

    added env

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:"mongodb://127.0.0.1:27017/storybooks",
+  googleClientID:"107698569510-edomo02jgeqr6k4hcqtl4kdhevnvrj17.apps.googleusercontent.com",
+  googleClientSecret:"jf3tFbZE1-WXMRRyqaBGFf9w"
+}
